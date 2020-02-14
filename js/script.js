f67c460050cffb1da1ff1dca8db2e8fc8e184e86
Brandon-Tang <btangsup@gmail.com>
2020-02-13 20:18:26 -0500

    added more functionality along with responsiveness

--- js/script.js
+++ js/script.js
@@ -1,32 +1,6 @@
-
-
-    const results = [
-        // {
-            // title: "Redbone",
-            // lyrics: "Too late, you wanna make it right but now its too late. My peanut butter chocolate cake with kool-aid. I'm trying not to waste my time..."
-        // },
-        // {
-        //     title: "3005",
-        //     lyrics: "Got no patience cause I'm not a doctor. Girl why is you lyin, girl why you Mufasa, ye micasa su casa, gotta strip it like Gaza. Got so high off volcanoes, now the flow is so lava..."
-        // },
-        // {
-        //     title: "Bonfire",
-        //     lyrics: "In Adidas with some short shorts, B - O - O all over me. My green is where it's supposed to be, your green is in my grocery. This Asian dude, I stole his girl, and now he got that Kogi beef. My **** is like an accent mark, it's all about the over Es..."
-        // },
-        // {
-        //     title: "Sober",
-        //     lyrics: "And now that it's over, I'll never be sober, I couldn't believe. Now I'm so high."
-        // },
-        // {
-        //     title: "This is America",
-        //     lyrics: "Look how I'm geekin, I'm so fitted. I'm on Gucci. I'm so pretty. I'm gon' get it. Watch me move. This a celly. That's a tool"
-        // }
-    ]
-
 $('document').ready(function(){
 
-    // interactive data to be placed when clicked on 
-
+    const results = []
     const allQuestions = [
         {
             question: "Which lion king character best represents you?",
@@ -37,8 +11,6 @@ $('document').ready(function(){
                 "Scar", //bonfire
                 "Rafiki" //3005
             ],
-            questionImg: '../assets/questionImg1.jpg',
-            altText: "Lion King Charcaters",
         },
         {   
             questionNumb: "Question 2",
@@ -50,8 +22,6 @@ $('document').ready(function(){
                 "Smores", //bonfire
                 "Pokebowl" // 3005
             ],
-            questionImg: "../assets/questionImg2.png",
-            altText: "Childish Gambino Eating Pencils"
         },
         {   
             questionNumb: "Question 3",
@@ -63,8 +33,6 @@ $('document').ready(function(){
                 "Flex 💪", // redbone
                 "Mind blown 🤯" // sober
             ],
-            questionImg: "../assets/questionImg3.png",
-            altText: "Childish gambino looking real surprised"
         },
         {
             questionNumb: "Question 4",
@@ -76,12 +44,10 @@ $('document').ready(function(){
                 "Outdoors", //bonfire
                 "Travelling around the world" // this is america
             ],
-            questionImg: "../assets/questionImg4.jpg",
-            altText: "Childish Gambino sitting with a teddy-bear on a ferris wheel"
         },
         {
             questionNumb: "Question 5",
-            question: "What's music mood best describes you?",
+            question: "Which music mood best describes you?",
             options: [
                 "I'll rap while wrapping tortilla wraps", //bonfire
                 "I infintely sound better when I sing in the shower", //redbone
@@ -92,19 +58,20 @@ $('document').ready(function(){
         },
     ];
 
-    // start How Childish are you Quiz!!!
-
-    // const sectionScroll = function(sectionName){
-    //         $('html').animate({
-    //             let section = $('<section>').first(),
-    //             $('<section>').last().text("scrollTop:" + section.scrollTop()), 
-    //         }, 'slow');
-    // };
-
-    $("#submit0").click(function () {
+    // Start the quiz!
+    $(".startQuizBtn").click(function () {
         $('html, .quiz1').animate({
             scrollTop: $(".quiz1").offset().top
-        }, 'slow'); 
+        }); 
+    });
+
+    // transition to next question
+    $(".submitButton").click(function () {
+        const offsetTop = $(this).offset().top + 100;
+        $('html, .quiz2').animate({
+
+            scrollTop: offsetTop
+        }, 'slow');
     });
 
     // score counter for which song is clicked the most
@@ -154,67 +121,45 @@ $('document').ready(function(){
             three005++;
         } else {
             bonfire++;
-        }
-
-        console.log(sober, thisIsAmerica, redbone, three005, bonfire);
-        // $(".submit1").click(function () {
-        //     sectionScroll("submit2");
-        // });
-
-        $("#submit1").click(function () {
-            $('html, .quiz2').animate({
-                scrollTop: $(".quiz2").offset().top
-            }, 'slow');
-        });
-
+        }  
     })
-
 
     //Question 2
     
     $('#question2').on('submit', function (event) {
 
-            event.preventDefault();
+        //prevent default action from loading a new page
+        event.preventDefault();
 
-            const question2 = $('input[name=question2]:checked').val();
+        const question2 = $('input[name=question2]:checked').val();
 
-            const result = {};
-            result.title = question2;
-            result.question2Value = question2;
-            results.push(result);
-            console.log(results);
+        const result = {};
+        result.title = question2;
+        result.question2Value = question2;
+        results.push(result);
 
+        const questionNumb = allQuestions[2].questionNumb;
+        const question = allQuestions[2].question;
 
-            const questionNumb = allQuestions[2].questionNumb;
-            const question = allQuestions[2].question;
+        $('.quiz3 h2').empty();
+        $('.quiz3 h3').empty();
 
-            $('.quiz3 h2').empty();
-            $('.quiz3 h3').empty();
+        $('.quiz3').css('opacity', '1');
+        $('.quiz3 h2').append(questionNumb);
+        $('.quiz3 h3').append(question);
 
-            $('.quiz3').css('opacity', '1');
-            $('.quiz3 h2').append(questionNumb);
-            $('.quiz3 h3').append(question);
-
-            if (question2 === 'sober') {
-                sober++;
-            } else if (question2 === 'thisIsAmerica') {
-                thisIsAmerica++;
-            } else if (question2 === 'redbone') {
-                redbone++;
-            } else if (question2 === 'three005') {
-                three005++;
-            } else {
-                bonfire++;
-            }
-        console.log(sober, thisIsAmerica, redbone, three005, bonfire);
-
-            $('#submit2').click(function (){
-                $('html, .quiz3').animate({
-                    scrollTop: $(".quiz3").offset().top
-                }, 'slow');
-            })
+        if (question2 === 'sober') {
+            sober++;
+        } else if (question2 === 'thisIsAmerica') {
+            thisIsAmerica++;
+        } else if (question2 === 'redbone') {
+            redbone++;
+        } else if (question2 === 'three005') {
+            three005++;
+        } else {
+            bonfire++;
+        }
     })
-
 
     //Question 3
     
@@ -228,9 +173,7 @@ $('document').ready(function(){
         result.title = question3;
         result.question3Value = question3;
         results.push(result);
-        console.log(results);
         
-
         const questionNumb = allQuestions[3].questionNumb;
         const question = allQuestions[3].question;
 
@@ -252,15 +195,7 @@ $('document').ready(function(){
         } else {
             bonfire++;
         }
-        console.log(sober, thisIsAmerica, redbone, three005, bonfire);
-
-        $('#submit3').click(function () {
-            $('html, .quiz4').animate({
-                scrollTop: $(".quiz4").offset().top
-            }, 'slow');
-        })
     })
-
 
     //Question 4
 
@@ -273,7 +208,6 @@ $('document').ready(function(){
         result.title = question4;
         result.question4Value = question4;
         results.push(result);
-        console.log(results);
 
         const questionNumb = allQuestions[4].questionNumb;
         const question = allQuestions[4].question;
@@ -296,14 +230,7 @@ $('document').ready(function(){
         } else {
             bonfire++;
         }
-        console.log(sober, thisIsAmerica, redbone, three005, bonfire);
-        $('#submit4').click(function () {
-            $('html, .quiz4').animate({
-                scrollTop: $(".quiz4").offset().top
-            }, 'slow');
-        })
     })
-
 
     //Question 5
 
@@ -317,7 +244,6 @@ $('document').ready(function(){
         result.title = question5;
         result.question5Value = question5;
         results.push(result);
-        console.log(results);
 
         if (question5 === 'sober') {
             sober++;
@@ -330,8 +256,6 @@ $('document').ready(function(){
         } else {
             bonfire++;
         }
-
-        console.log(sober, thisIsAmerica, redbone, three005, bonfire);
 
         // created an if/else statement to show for results, whichever total value gets selected the most will display. need to create a tie if else statement
 
@@ -351,11 +275,15 @@ $('document').ready(function(){
             bonfire = 'Bonfire';
             $('.songTitleResult').append(`Your Childish Gambino Song is ${bonfire}`);
         }
+
+        console.log(sober, thisIsAmerica, redbone, bonfire, three005);
     })
-    
-
-    console.log(sober, thisIsAmerica, redbone, three005, bonfire);
-
+    $("#restart").on('click', function (event) {
+        event.preventDefault();
+        location.reload(true);
+    });
 });
+
+   
 
 
